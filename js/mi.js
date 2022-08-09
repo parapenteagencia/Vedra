3c0c813994815f80a20e65d8b848957434c3bee0
Luis Angel Hernandez <luis@parapenteagencia.com>
2022-08-09 17:34:04 -0500

    Primera version

---
+++ js/mi.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	/* Slider */
+     $('.galeria').bxSlider({
+        mode: 'fade',
+        captions: true,
+        slideWidth: 714,
+        pager: false,
+        auto: true
+     });
+});
